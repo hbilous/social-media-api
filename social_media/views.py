5eabd7f1aa8c3126d7ae3e0d54b2300959ce6d4f
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-03-19 18:12:41 +0200

    implemented views and serializers to social_media

--- social_media/views.py
+++ social_media/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from social_media.models import Post, Like, Comment
-from social_media.serializers import PostSerializer, LikeSerializer
+from social_media.serializers import PostSerializer, LikeSerializer, CommentSerializer
 
 
 class PostViewSet(viewsets.GenericViewSet):
