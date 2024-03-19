d9f06d7a405238ad5b386eb32d9aac4a9f381b33
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-03-19 20:10:04 +0200

    fixed views

--- social_media/views.py
+++ social_media/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from social_media.models import Post, Like, Comment
 from social_media.serializers import PostSerializer, LikeSerializer, CommentSerializer
 
 
-class PostViewSet(viewsets.GenericViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class LikeViewSet(viewsets.GenericViewSet):
+class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
 
