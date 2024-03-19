495879bb81ea72eb34821bdf5d70b3aba0b65d99
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-03-19 18:41:03 +0200

    added admin and urls for social_media

---
+++ social_media/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from social_media.views import PostViewSet, LikeViewSet, CommentViewSet
+
+router = routers.DefaultRouter
+router.register("posts", PostViewSet)
+router.register("likes", LikeViewSet)
+router.register("comments", CommentViewSet)
+
+urlpattern = [path("", include(router.urls))]
+
+app_name = "social_media"
